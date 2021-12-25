96a4c50e61432da604da05d5cffb0b0c6d7e12ed
vocongly <vocongly05042001@gmail.com>
2021-12-25 10:22:33 +0700

    Play Custom

--- app/src/main/java/com/example/nhom10_laptrinhgame2048/PlayCustome.java
+++ app/src/main/java/com/example/nhom10_laptrinhgame2048/PlayCustome.java
@@ -6,43 +6,82 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PlayCustome extends AppCompatActivity implements View.OnClickListener {
-    Button btn4x4,btn5x5,btn6x6,btn8x8;
+    Button btn4x4,btn5x5,btn6x6,btn8x8,btnnewgamecustom;
+    ImageButton btnhomemain;
+    ImageView ivback,ivnext,imggame;
+    TextView tvnamegame;
+    int count,size;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_custome);
+        count=0;
+        size=4;
         init();
     }
     private void init(){
-        btn4x4 = findViewById(R.id.btn4x4);
-        btn5x5 = findViewById(R.id.btn5x5);
-        btn6x6 = findViewById(R.id.btn6x6);
-        btn8x8 = findViewById(R.id.btn8x8);
-        btn4x4.setOnClickListener(this);
-        btn5x5.setOnClickListener(this);
-        btn6x6.setOnClickListener(this);
-        btn8x8.setOnClickListener(this);
-    }
+        ivback = findViewById(R.id.ivBack);
+        ivnext = findViewById(R.id.ivNext);
+        imggame = findViewById(R.id.imgGame);
+        tvnamegame = findViewById(R.id.tvNameGame);
+        ivback.setOnClickListener(this);
+        ivnext.setOnClickListener(this);
 
+        btnhomemain= findViewById(R.id.btnHomeCustom);
+        btnhomemain.setOnClickListener(this);
+
+        btnnewgamecustom = findViewById(R.id.btnNewGameCustom);
+        btnnewgamecustom.setOnClickListener(this);
+    }
+    private void getCount(){
+        if(count > DataGame.getDataGame().arrImage.length -1){
+            count =0;
+        }else if (count < 0){
+            count = DataGame.getDataGame().arrImage.length -1;
+        }
+    }
+    private int setSize(int count){
+        int size = 0;
+        switch (count){
+            case 0:
+                size =4;
+                break;
+            case 1:
+                size =5;
+                break;
+            case 2:
+                size = 6;
+                break;
+            case 3:
+                size =8;
+        }
+        return size;
+    }
     @Override
     public void onClick(View view) {
-        if(view.getId() == btn4x4.getId()){
+        if(view == ivback){
+            count--;
+        }
+        if(view == ivnext){
+            count++;
+        }
+        getCount();
+        size = setSize(count);
+        imggame.setImageResource(DataGame.getDataGame().arrImage[count]);
+        tvnamegame.setText(DataGame.getDataGame().arrName[count]);
+
+        if(view == btnnewgamecustom){
             Intent intent = new Intent(PlayCustome.this,MainActivity.class);
-            intent.putExtra("size",4);
+            intent.putExtra("size",size);
             startActivity(intent);
-        } else if(view.getId() == btn5x5.getId()){
-            Intent intent = new Intent(PlayCustome.this,MainActivity.class);
-            intent.putExtra("size",5);
-            startActivity(intent);
-        } else if(view.getId() == btn6x6.getId()){
-            Intent intent = new Intent(PlayCustome.this,MainActivity.class);
-            intent.putExtra("size",6);
-            startActivity(intent);
-        }else if(view.getId() == btn8x8.getId()){
-            Intent intent = new Intent(PlayCustome.this,MainActivity.class);
-            intent.putExtra("size",8);
+        }
+        else if(view == btnhomemain){
+            Intent intent = new Intent(PlayCustome.this,MenuScreenActivity.class);
             startActivity(intent);
         }
     }
