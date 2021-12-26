afd58123ffb7a43557aa1fb0a19451b8fb8a5c41
vocongly <vocongly05042001@gmail.com>
2021-12-26 23:29:22 +0700

    Ly push PlayCustom ne

--- app/src/main/java/com/example/nhom10_laptrinhgame2048/GameScore.java
+++ app/src/main/java/com/example/nhom10_laptrinhgame2048/GameScore.java
@@ -3,7 +3,7 @@ package com.example.nhom10_laptrinhgame2048;
 import java.io.Serializable;
 import java.util.Date;
 
-public class GameScore implements Serializable {
+public class GameScore implements Comparable {
     private String name;
     private int score;
 
@@ -18,5 +18,11 @@ public class GameScore implements Serializable {
 
     public int getScore() {
         return score;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        GameScore g = (GameScore) o;
+        return this.name.compareTo(g.name);
     }
 }
--- app/src/main/java/com/example/nhom10_laptrinhgame2048/HistoryActivity.java
+++ app/src/main/java/com/example/nhom10_laptrinhgame2048/HistoryActivity.java
@@ -2,13 +2,22 @@ package com.example.nhom10_laptrinhgame2048;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
-public class HistoryActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class HistoryActivity extends AppCompatActivity implements View.OnClickListener {
     SQLiteHelper helper;
     ListView lvScores;
-
+    ImageButton btnhomemain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +29,24 @@ public class HistoryActivity extends AppCompatActivity {
     private void initDatabase(){
         helper = new SQLiteHelper(this);
         helper.createTable();
+
+        btnhomemain = findViewById(R.id.btnHomeMain);
+        btnhomemain.setOnClickListener(this);
     }
 
     private void LoadListView(){
         lvScores = findViewById(R.id.lvScores);
-        ListViewAdapter adapter = new ListViewAdapter(HistoryActivity.this,0,helper.getListScore());
+        List<GameScore> list = helper.getListScore();
+        Collections.sort(list);
+        ListViewAdapter adapter = new ListViewAdapter(HistoryActivity.this,0, list);
         lvScores.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnhomemain){
+            Intent intent = new Intent(HistoryActivity.this, MenuScreenActivity.class);
+            startActivity(intent);
+        }
     }
 }
